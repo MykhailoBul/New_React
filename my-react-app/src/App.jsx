92aa4ae29fd89c21d51b7029397421ae67828414
MykhailoBul <mykhailo.bulava@voco.ee>
2025-12-27 15:36:14 +0200

    Initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import ExpenseDate from './components/ExpenseDate';
+
+function App() {
+  return (
+    <div className=''>
+      <ExpenseDate date={new Date()} />
+      
+    </div>
+  );
+}
+
+export default App;
