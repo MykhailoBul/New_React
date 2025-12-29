4c268fa6ef0fbaaaa2d22039806a59acca5433bd
MykhailoBul <mykhailo.bulava@voco.ee>
2025-12-29 14:52:32 +0200

    done

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,18 @@
-import ExpenseDate from './components/ExpenseDate';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
-    <div className=''>
-      <ExpenseDate date={new Date()} />
-      
+    <div>
+      <ExpenseItem
+        date={new Date(2023, 0, 10)}
+        title="New book"
+        price="30.99"
+      />
+      <ExpenseItem
+        date={new Date(2023, 0, 10)}
+        title="New jeans"
+        price="99.99"
+      />
     </div>
   );
 }
