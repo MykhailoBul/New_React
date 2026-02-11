5bcecf33df9562ad4960276f01e00d9f230fbe49
MykhailoBul <mykhailo.bulava@voco.ee>
2026-02-11 14:34:51 +0200

    17a\

--- my-react-app/src/components/NewExpense/ExpenseForm.jsx
+++ my-react-app/src/components/NewExpense/ExpenseForm.jsx
@@ -1,57 +1,83 @@
 import './ExpenseForm.css'
-import { useState, useRef } from 'react'
-
+import { useRef, useState } from 'react'
+import Error from '../UI/Error'
 
 const ExpenseForm = (props) => {
+    const [error, setError] = useState(null)
+
     const titleInputRef = useRef()
     const priceInputRef = useRef()
     const dateInputRef = useRef()
 
+    const errorHandler = () => {
+        setError(null)
+    }
+
     const submitHandler = (event) => {
         event.preventDefault()
+        const enteredTitle = titleInputRef.current.value
+        const enteredPrice = priceInputRef.current.value
+        const enteredDate = dateInputRef.current.value
+
+        if (enteredTitle.trim().length === 0 || 
+        enteredPrice.trim().length === 0 || 
+        enteredDate.trim().length === 0) {
+            setError({
+                title: 'Invalid input',
+                message: 'Please enter a valid title, price, and date (non-empty values).'
+            })
+            return
+        }
+
         const expenseData = {
-            title: titleInputRef.current.value,
-            price: priceInputRef.current.value,
-            date: new Date(dateInputRef.current.value)
+            title: enteredTitle,
+            price: enteredPrice,
+            date: new Date(enteredDate)
         }
         props.onSaveExpenseData(expenseData)
-        setuserInput({
-            enteredTitle: '',
-            enteredPrice: '',
-            enteredDate: ''
-        })
+
+        titleInputRef.current.value = ''
+        priceInputRef.current.value = ''
+        dateInputRef.current.value = ''
     }
+    
+    console.log(error)
+
     return(
+        <>
+        {error && <Error title={error.title} message={error.message} onConfirm={errorHandler}/>}
         <form onSubmit={submitHandler}>
-            <div className='new-expense__controls'>
-                <div className='new-expense_control'>
-                    <label>Title</label>
-                    <input type="text" ref={titleInputRef}/>
-                </div>
-                <div className='new-expense_control'>
-                    <label>Price</label>
-                    <input type="number" min='0.01' step='0.01' ref={priceInputRef}  
-                    value={userInput.enteredPrice}/>
-                </div>
-                <div className='new-expense_control'>
-                    <label>Date</label>
-                    <input type="date" min='2025-11-12' max='2026-01-31' ref={dateInputRef} 
-                    value={userInput.enteredDate}
-                    />
-                </div>
-            </div>
-            <div className="new-expense__actions">
-                <button
-                    type="button"
-                    onClick={props.onCancel}>Cancel
-                </button>
+                <div className='new-expense__controls'>
 
-                <button type="submit">
-                    Add Expense
-                </button>
-            </div>
+                    <div className='new-expense_control'>
+                        <label>Title</label>
+                        <input type="text" ref={titleInputRef}/>
+                    </div>
 
-        </form>
+                    <div className='new-expense_control'>
+                        <label>Price</label>
+                        <input
+                            type="number"
+                            min='0.01'
+                            step='0.01'
+                            ref={priceInputRef}/>
+                    </div>
+
+                    <div className='new-expense_control'>
+                        <label>Date</label>
+                        <input
+                            type="date"
+                            min='2023-01-01'
+                            max='2026-12-31'
+                            ref={dateInputRef}/>
+                    </div>
+                </div>
+                <div className="new-expense__actions">
+                    <button type="button" onClick={props.onCancel}>Cancel</button>
+                    <button type="submit">Add Expense</button>
+                </div>
+            </form>
+        </>
     )
 }
 
---
+++ my-react-app/src/components/UI/Button.jsx
@@ -0,0 +1,11 @@
+import './Button.css'
+
+const Buttons = (props) => {
+    return (
+        <button className='button' type={props.type || 'button'} 
+        onClick={props.onClick}> {props.children}
+        </button>
+    )
+}
+
+export default Buttons
